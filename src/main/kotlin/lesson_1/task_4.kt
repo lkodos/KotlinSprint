f08cfb4753924d49cee9a20b4a70f307c9597ba3
lkodos <tshagimardanov@gmail.com>
2025-02-06 21:49:25 +0300

    KS-2-2 fix remark

--- src/main/kotlin/lesson_1/task_4.kt
+++ src/main/kotlin/lesson_1/task_4.kt
@@ -31,4 +31,11 @@ fun main() {
     val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
+
+    println("Расстояние: $length")
+    println("Возраст: $age")
+    println("Часть дня: $partOfDay")
+    println("Секунды: $seconds")
+    println("Часть года: $partOfYear")
+    println("Апогей: $apogee")
 }
